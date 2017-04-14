e31af6147b4fbe250ec6fd269384c559bceccb3f
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-04-14 11:49:38 +0900

    setting vue-onsenui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+import 'onsenui';
+
 import Vue from 'vue';
+import VueOnsen from 'vue-onsenui';
 import App from './App';
+
+Vue.use(VueOnsen);
 
 /* eslint-disable no-new */
 new Vue({
