d8d0551285f8cb8418e0aaeacaf71c2aa0c33b1f
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-04-14 16:21:38 +0900

    open device camera

--- src/components/CameraPage.vue
+++ src/components/CameraPage.vue
@@ -2,16 +2,38 @@
   <v-ons-page>
     <custom-toolbar>Camera Page</custom-toolbar>
     <p style="text-align: center">This is the second page</p>
+    <p style="text-align: center">
+      This is the camera open
+      <v-ons-button @click="takePictures">camera</v-ons-button>
+    </p>
   </v-ons-page>
 </template>
 
 <script>
 import CustomToolbar from './CustomToolbar';
 
+function cameraSuccess() {
+  // const img = document.getElementById('image');
+  // img.src = 'data:image/jpeg;base64,' + image;
+  alert('Succes!');
+}
+
+function cameraError() {
+  alert('Failed!:');
+}
+
+function takePictures() {
+  navigator.camera.getPicture(cameraSuccess, cameraError, { quality: 80 });
+}
+
 export default {
   name: 'camera-page',
   components: {
     CustomToolbar,
   },
+  methods: {
+    takePictures,
+  },
 };
+
 </script>
