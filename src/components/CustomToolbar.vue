1d25e2ba9c4d7868bf93b6704d94f8a06b26e8f8
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-04-14 12:05:42 +0900

    create CustomToolbar component

---
+++ src/components/CustomToolbar.vue
@@ -0,0 +1,15 @@
+<template class="custom-toolbar">
+  <v-ons-toolbar>
+    <div class="left">
+      <v-ons-back-button>{{ backLabel }}</v-ons-back-button>
+    </div>
+    <div class="center"><slot></slot></div>
+  </v-ons-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'custom-toolbar',
+  props: ['backLabel'],
+};
+</script>
