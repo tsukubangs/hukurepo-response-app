a47d94172796e8e20b2f8170000c77779a8dca85
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-04-14 13:26:29 +0900

    create Camara Page

---
+++ src/components/CameraPage.vue
@@ -0,0 +1,17 @@
+<template id="camera-page">
+  <v-ons-page>
+    <custom-toolbar :back-label="'Top Page'">Camera Page</custom-toolbar>
+    <p style="text-align: center">This is the second page</p>
+  </v-ons-page>
+</template>
+
+<script>
+import CustomToolbar from './CustomToolbar';
+
+export default {
+  name: 'top-page',
+  components: {
+    CustomToolbar,
+  },
+};
+</script>
