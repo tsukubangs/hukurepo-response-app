511698aa8641b0eb451ac534970ea20db31fa5ca
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-04-19 14:21:04 +0900

    get photo from devise

--- src/components/CameraPage.vue
+++ src/components/CameraPage.vue
@@ -7,6 +7,13 @@
       <v-ons-button @click="takePictures">Camera</v-ons-button>
     </p>
     <img :src="image" style="width:100%;"/>
+
+    <p style="text-align: center">
+      Get an image from a photo album of the device
+      <v-ons-button @click="getPhoto">from devise</v-ons-button>
+    <img style="display:none;" id="picture" src="" width="150" height="150" />
+    </p>
+
   </v-ons-page>
 </template>
 
@@ -24,6 +31,28 @@ function takePictures() {
   }, cameraError, { quality: 80 });
 }
 
+// function onDeviceReady() {
+//  window.alert('Loading Cordova is completed');
+// }
+
+function onSuccess(imageURI) {
+  const largeImage = document.getElementById('picture');
+  largeImage.style.display = 'block';
+  largeImage.src = imageURI;
+}
+
+function onFail(message) {
+  alert(`An error occured: ${message}`);
+}
+
+function getPhoto() {
+// Specify the source to get the photos.
+  navigator.camera.getPicture(onSuccess, onFail,
+    { quality: 50,
+      destinationType: navigator.camera.DestinationType.FILE_URI,
+      sourceType: navigator.camera.PictureSourceType.SAVEDPHOTOALBUM });
+}
+
 export default {
   name: 'camera-page',
   components: {
@@ -36,6 +65,7 @@ export default {
   },
   methods: {
     takePictures,
+    getPhoto,
   },
 };
 
