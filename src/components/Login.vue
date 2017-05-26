0c89c438de404e3b3bd81887b9d8550c4c2c6606
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-05-26 14:48:27 +0900

    add login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,33 @@
 <template>
-    <p>
-        login page
-    </p>
+  <v-ons-page>
+    <div id="login-form">
+      <p><ons-input id="username" modifier="underbar" placeholder="Username" float></ons-input></p>
+      <p><ons-input id="password" modifier="underbar" type="password" placeholder="Password" float></ons-input></p>
+      <p style="margin-top: 30px;">
+        <ons-button @click="login()">Login</ons-button>
+      </p>
+    </div>
+  </v-ons-page>
 </template>
 
 <script>
 export default {
   name: 'login-page',
+  methods: {
+    login() {
+
+    },
+  },
 };
 </script>
+
+<style>
+#login-form {
+  background-color: #FFF;
+  width: 250px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
