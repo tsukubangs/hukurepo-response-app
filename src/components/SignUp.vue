bb88ebde9f5d4c19358370b3127da2f6d298ca54
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-05-31 17:33:01 +0900

    add gender

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -31,6 +31,27 @@
           </v-ons-input>
         </div>
       </v-ons-list-item>
+      <v-ons-list-header>
+        Gender
+      </v-ons-list-header>
+      <v-ons-list-item v-for="(gender, $index) in genders" tappable>
+        <label class="left">
+          <v-ons-input type="radio" :input-id="'radio-' + $index" :value="gender" v-ons-model="selectedGender"></v-ons-input>
+        </label>
+        <label :for="'radio-' + $index" class="center">{{ gender }}</label>
+      </v-ons-list-item>
     </v-ons-list>
   </v-ons-page>
 </template>
+
+<script>
+export default {
+  name: 'sign-up',
+  data() {
+    return {
+      selectedGender: 'male',
+      genders: ['male', 'female'],
+    };
+  },
+};
+</script>
