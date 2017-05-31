0c0838f814e515cb1bd9bd85171fbe2493ac09cb
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-05-31 17:07:03 +0900

    get user info

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -5,7 +5,7 @@
       This is the first page
       <v-ons-button @click="push">POST</v-ons-button>
     </p>
-    <ons-button @click="getUser()">get user info</ons-button>
+    <p>{{email}}</p>
   </v-ons-page>
 </template>
 
@@ -21,25 +21,31 @@ export default {
   components: {
     CustomToolbar,
   },
-  methods: {
-    push() {
-      this.pageStack.push(CameraPage);
-    },
-    getUser() {
-      const token = window.localStorage.getItem('access_token');
-      console.log(token);
-      const config = {
-        headers: { Authorization: token },
-      };
-      axios.get(`${WEB_API_URL}/v1/users`, config)
+  data() {
+    return {
+      email: '',
+    };
+  },
+  created() {
+    const token = window.localStorage.getItem('access_token');
+    console.log(token);
+    const config = {
+      headers: { Authorization: token },
+    };
+    axios.get(`${WEB_API_URL}/v1/users`, config)
           .then((response) => {
             console.log(response);
+            this.email = response.data[0].email;
           }).catch(() => {
             ons.notification.alert({
               title: 'failed',
               message: 'Sorry, Pleas Sign in again.',
             });
           });
+  },
+  methods: {
+    push() {
+      this.pageStack.push(CameraPage);
     },
   },
   props: ['pageStack'],
