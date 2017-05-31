4be40b63869e6373a2f36cde206fdc57480113db
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-05-31 17:06:49 +0900

    add email and password input

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,7 +1,40 @@
 <template>
   <v-ons-page>
-    <div id="sign-up">
-        tes
-    </div>
+    <v-ons-toolbar>
+      <div class="center">Sign up</div>
+    </v-ons-toolbar>
+    <v-ons-list>
+      <v-ons-list-header>
+          Email
+      </v-ons-list-header>
+      <v-ons-list-item>
+        <div class="center">
+          <v-ons-input placeholder="Email" float>
+          </v-ons-input>
+        </div>
+      </v-ons-list-item>
+    </v-ons-list>
+    <v-ons-list>
+      <v-ons-list-header>
+          Password
+      </v-ons-list-header>
+      <v-ons-list-item>
+        <div class="center">
+          <v-ons-input placeholder="Password" type="password" float>
+          </v-ons-input>
+        </div>
+      </v-ons-list-item>
+    </v-ons-list>
+    <v-ons-list>
+      <v-ons-list-header>
+          Confirm Password
+      </v-ons-list-header>
+      <v-ons-list-item>
+        <div class="center">
+          <v-ons-input placeholder="Confirm Password" type="password" float>
+          </v-ons-input>
+        </div>
+      </v-ons-list-item>
+    </v-ons-list>
   </v-ons-page>
 </template>
