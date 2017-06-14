2a5411d033e49b1780ccd1d1898c363695293212
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-06-14 15:14:42 +0900

    add vuex

---
+++ src/vuex/store.js
@@ -0,0 +1,16 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+const state = {};
+const actions = {};
+const getters = {};
+const mutations = {};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations,
+});
