6c45592125802dcb6425bffb75216fc989ac7a51
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-07-05 18:49:53 +0900

    add ResponseCard compornent & add css styles

---
+++ src/components/ResponseCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-ons-card>
+    <div class="content">
+      <div class="comment">
+        {{ response.comment }}
+      </div>
+      <div class="date">
+        {{ response.created_at }}
+      </div>
+    </div>
+  </v-ons-card>
+</template>
+
+<script>
+export default {
+  name: 'response-card',
+  props: [
+    'response',
+  ],
+};
+</script>
+
+<style scoped>
+v-ons-card {
+  position: relative;
+  display: flex;
+  background: rgba(1,168,236,0.1);
+  border-radius: 20px;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  padding: 0 10px;
+  margin: 5px;
+  text-align: left;
+}
+.comment {
+  display: flex;
+  /*align-items: center;*/
+  /*justify-content: center;*/
+  flex-grow: 9;
+  color: #7f7f7f;
+}
+.date {
+  flex-grow: 1;
+  text-align: right;
+  font-size: x-small;
+  color: #aaaaaa;
+}
+.cover {
+  width: 100%;
+  height: 100%;
+}
+p {
+  padding: 0.5em 0;
+  margin: 0;
+  font-size: initial;
+}
+</style>
--- src/components/ResponsePage.vue
+++ src/components/ResponsePage.vue
@@ -1,9 +1,17 @@
 <template lang="html">
   <v-ons-page>
     <custom-toolbar>Response Page</custom-toolbar>
-      <p>this is a response page</p>
-      <p>{{ selectedProblem }}</p>
-      <p>{{ responses }}</p>
+    <main class="h100">
+      <ul class="card-list">
+        <li>
+          <response-card :response="selectedProblem" class="card card-right"></response-card>
+        </li>
+        <li v-for="response in responses">
+          <!-- <problem-card :problem="problem" class="card"></problem-card> -->
+          <response-card :response="response" class="card"></response-card>
+        </li>
+      </ul>
+    </main>
   </v-ons-page>
 </template>
 
@@ -12,12 +20,14 @@ import { mapGetters } from 'vuex';
 import axios from 'axios';
 // import ons from 'onsenui';
 import CustomToolbar from './CustomToolbar';
+import ResponseCard from './ResponseCard';
 import { WEB_API_URL } from '../../.env';
 // import { FETCH_PROBLEMS } from '../vuex/mutation-types';
 export default {
   name: 'response-page',
   components: {
     CustomToolbar,
+    ResponseCard,
   },
   data() {
     return {
@@ -37,7 +47,7 @@ export default {
     axios.get(`${WEB_API_URL}/v1/problems/${this.selectedProblem.id}/responses`, config)
       .then((response) => {
         console.log(response);
-        this.responses = (response.data);
+        this.responses = response.data;
       })
       .catch((error) => {
         console.log(error);
@@ -47,5 +57,27 @@ export default {
 };
 </script>
 
-<style lang="css">
+<style scoped>
+.h100 {
+  height:100%;
+}
+main {
+  padding: 5px;
+  box-sizing: border-box;
+}
+.card {
+  width: 80%;
+}
+.card-right {
+  margin-left: auto;
+}
+.card-list {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  padding-bottom: 100px;
+}
+.card-list > li {
+  margin: 10px 0;
+}
 </style>
