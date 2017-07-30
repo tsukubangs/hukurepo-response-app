98ed52688de9013a678510395e57f1f0d4ae208a
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2017-07-30 23:26:17 +0900

    add tsukuba's icon

--- src/components/ResponseCard.vue
+++ src/components/ResponseCard.vue
@@ -1,7 +1,10 @@
 <template>
   <v-ons-card v-bind:class="{ reverce: !isMyResponse }">
     <div class="icon-container">
-      <img src="../assets/response_icon.png" />
+      <div class="tsukuba-icon" v-if="!isMyResponse">
+        <img src="../assets/s_logo.png" />
+      </div>
+      <img src="../assets/response_icon.png" v-else />
     </div>
     <div class="content">
       <p>{{ response.comment }}</p>
@@ -34,7 +37,21 @@ v-ons-card {
   width: 120px;
 }
 .icon-container > * {
+  height: 50px;
   width: 50px;
+}
+.tsukuba-icon {
+  background-color: #01a8ec;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+}
+.tsukuba-icon > img {
+  display: inline-block;
+  height: unset;
+  width: 30px;
 }
 .content {
   width: 100%;
