7d9e8eb3ea66f6e68263a9c71aff0c183a5bd1e3
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2017-08-19 10:57:58 +0900

    send device ID to server on deviceREADY

--- src/main.js
+++ src/main.js
@@ -7,13 +7,15 @@ import ons from 'onsenui';
 import Vue from 'vue';
 import VueOnsen from 'vue-onsenui';
 import * as VueGoogleMaps from 'vue2-google-maps';
+import axios from 'axios';
 
 import './style.css';
 
 import router from './router';
 import App from './App';
-import { GOOGLE_MAP_API } from '../.env';
+import { GOOGLE_MAP_API, WEB_API_URL } from '../.env';
 import store from './vuex/store';
+
 
 Vue.use(VueOnsen);
 ons.forcePlatformStyling('ios');
@@ -65,15 +67,37 @@ const app = {
         console.log(`Error registering onNotification callback: ${err}`);
       },
     );
+
+    // FCMPlugin.onTokenRefresh( onTokenRefreshCallback(token) );
+    // Note that this callback will be fired everytime a new token is generated, including the first time.
+    FCMPlugin.onTokenRefresh((token) => {
+      alert(token);
+    });
   },
 
   /* eslint-disable no-undef */
   initPushNotification() {
+    const authorizationtoken = window.localStorage.getItem('access_token');
+    console.log('authorizationtoken');
+    console.log(authorizationtoken);
+    const config = {
+      headers: { Authorization: authorizationtoken },
+    };
+
     /* global FCMPlugin */
     FCMPlugin.getToken(
       (token) => {
+        console.log('DEVICE ID');
         console.log(token);
-    // ここにトークンを送るAPIの処理を書く
+        const data = {
+          device_token: token,
+        };
+    // トークンを送るAPIの処理
+        axios.put(`${WEB_API_URL}/v1/users/me/device_token`, data, config)
+        .catch(() => {
+          console.log('SUCCESS');
+          //
+        });
       },
       (err) => {
         console.log(`error retrieving token: ${err}`);
